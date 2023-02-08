166e70a0c8f0f976e8870b13e3c8616afb67dd00
Eric Flumerfelt <eflumerf@fnal.gov>
2023-02-08 10:29:28 -0600

    More development of the pub/sub version of the IOManager stress test

--- test/apps/iomanager_stress_test_pubsub.cxx
+++ test/apps/iomanager_stress_test_pubsub.cxx
@@ -77,6 +77,7 @@ struct TestConfig
   size_t message_size_kb = 1024;
   size_t num_runs = 1;
   size_t my_id = 0;
+  size_t send_interval_ms = 100;
   int publish_interval = 1000;
 
   void configure_connsvc()
@@ -88,7 +89,6 @@ struct TestConfig
   std::string get_connection_name(size_t app_id, size_t group_id, size_t conn_id)
   {
     std::stringstream ss;
-    ss << std::hex << std::internal << std::showbase << std::setw(10) << std::setfill('0');
     ss << "conn_A" << app_id << "_G" << group_id << "_C" << conn_id;
     return ss.str();
   }
@@ -96,7 +96,6 @@ struct TestConfig
   {
 
     std::stringstream ss;
-    ss << std::hex << std::internal << std::showbase << std::setw(10) << std::setfill('0');
     ss << "conn_A" << app_id << "_G" << group_id << "_.*";
     return ss.str();
   }
@@ -192,6 +191,14 @@ struct SubscriberTest
       , is_group_subscriber(true)
     {
     }
+
+    std::string get_connection_name(TestConfig& config)
+    {
+      if (is_group_subscriber) {
+        return config.get_group_connection_name(config.my_id, group_id);
+      }
+      return config.get_connection_name(config.my_id, group_id, conn_id);
+    }
   };
   std::vector<std::shared_ptr<SubscriberInfo>> subscribers;
   TestConfig config;
@@ -203,10 +210,12 @@ struct SubscriberTest
 
   void receive(size_t run_number)
   {
+    TLOG_DEBUG(5) << "Getting QuotaReached sender";
     auto start = std::chrono::steady_clock::now();
     auto quota_sender = dunedaq::get_iom_sender<QuotaReached>(config.get_subscriber_quota_name());
     auto after_control = std::chrono::steady_clock::now();
 
+    TLOG_DEBUG(5) << "Setting up SubscriberInfo objects";
     for (size_t group = 0; group < config.num_groups; ++group) {
       subscribers.push_back(std::make_shared<SubscriberInfo>(group));
       for (size_t conn = 0; conn < config.num_connections_per_group; ++conn) {
@@ -214,6 +223,7 @@ struct SubscriberTest
       }
     }
 
+    TLOG_DEBUG(5) << "Adding callbacks for each subscriber";
     auto after_info = std::chrono::steady_clock::now();
     std::for_each(std::execution::par_unseq,
                   std::begin(subscribers),
@@ -222,7 +232,8 @@ struct SubscriberTest
                     auto recv_proc = [=](Data& msg) {
                       TLOG_DEBUG(3) << "Received message " << msg.seq_number << " with size " << msg.contents.size()
                                     << " bytes from connection "
-                                    << config.get_connection_name(msg.publisher_id, msg.group_id, msg.conn_id);
+                                    << config.get_connection_name(msg.publisher_id, msg.group_id, msg.conn_id) << " at "
+                                    << info->get_connection_name(config);
 
                       if (msg.contents.size() != config.message_size_kb * 1024 ||
                           msg.seq_number != info->last_sequence_received[msg.conn_id] + 1) {
@@ -232,22 +243,15 @@ struct SubscriberTest
                       info->msgs_received++;
 
                       if (info->msgs_received >= config.num_messages && !info->is_group_subscriber) {
+                        TLOG_DEBUG(3) << "Complete condition reached, sending QuotaReached message for " << info->get_connection_name(config);
                         QuotaReached q(config.my_id, info->group_id, info->conn_id, run_number);
                         quota_sender->send(std::move(q), Sender::s_block);
                         info->complete = true;
                       }
                     };
 
-                    std::shared_ptr<ReceiverConcept<Data>> receiver;
-
                     auto before_receiver = std::chrono::steady_clock::now();
-                    if (info->is_group_subscriber) {
-                      receiver =
-                        dunedaq::get_iom_receiver<Data>(config.get_group_connection_name(config.my_id, info->group_id));
-                    } else {
-                      receiver = dunedaq::get_iom_receiver<Data>(
-                        config.get_connection_name(config.my_id, info->group_id, info->conn_id));
-                    }
+                    auto receiver = dunedaq::get_iom_receiver<Data>(info->get_connection_name(config));
                     auto after_receiver = std::chrono::steady_clock::now();
                     receiver->add_callback(recv_proc);
                     auto after_callback = std::chrono::steady_clock::now();
@@ -258,6 +262,7 @@ struct SubscriberTest
                   });
     auto after_callbacks = std::chrono::steady_clock::now();
 
+    TLOG_DEBUG(5) << "Starting wait loop for receives to complete";
     bool all_done = false;
     while (!all_done) {
       size_t recvrs_done = 0;
@@ -265,22 +270,22 @@ struct SubscriberTest
         if (sub->complete.load())
           recvrs_done++;
       }
-      all_done = recvrs_done == config.num_groups * config.num_connections_per_group;
+      TLOG_DEBUG(6) << "Done: " << recvrs_done
+                    << ", expected: " << config.num_groups * config.num_connections_per_group;
+      all_done = recvrs_done >= config.num_groups * config.num_connections_per_group;
+      if (!all_done)
+        std::this_thread::sleep_for(1ms);
     }
     auto after_receives = std::chrono::steady_clock::now();
 
+    TLOG_DEBUG(5) << "Removing callbacks";
     for (auto& info : subscribers) {
-      std::shared_ptr<ReceiverConcept<Data>> receiver;
-      if (info->is_group_subscriber) {
-        receiver = dunedaq::get_iom_receiver<Data>(config.get_group_connection_name(config.my_id, info->group_id));
-      } else {
-        receiver =
-          dunedaq::get_iom_receiver<Data>(config.get_connection_name(config.my_id, info->group_id, info->conn_id));
-      }
+      auto receiver = dunedaq::get_iom_receiver<Data>(info->get_connection_name(config));
       receiver->remove_callback();
     }
     auto after_cleanup = std::chrono::steady_clock::now();
 
+    TLOG_DEBUG(5) << "Calculating statistics";
     int min_get_time = std::numeric_limits<int>::max();
     int max_get_time = 0;
     int sum_get_time = 0;
@@ -288,6 +293,7 @@ struct SubscriberTest
     int max_cb_time = 0;
     int sum_cb_time = 0;
     for (auto& info : subscribers) {
+      TLOG_DEBUG(6) << "Received " << info->msgs_received << " messages at " << info->get_connection_name(config) << " (" << info->msgs_with_error << " size/sequence errors)";
       auto get_time = info->get_receiver_time.count();
       auto cb_time = info->add_callback_time.count();
       if (get_time > max_get_time)
@@ -306,6 +312,7 @@ struct SubscriberTest
 
     std::string info_file_name = config.info_file_base + "_receiver.csv";
 
+    TLOG_DEBUG(5) << "Writing to output CSV file " << info_file_name;
     struct stat buffer;
     bool file_exists = stat(info_file_name.c_str(), &buffer) == 0;
 
@@ -331,6 +338,7 @@ struct SubscriberTest
        << average_cb_time << std::endl;
 
     subscribers.clear();
+    TLOG_DEBUG(5) << "receive() done";
   }
 };
 
@@ -340,6 +348,7 @@ struct PublisherTest
   {
     size_t conn_id;
     size_t group_id;
+    size_t messages_sent{ 0 };
     std::shared_ptr<SenderConcept<Data>> sender;
     std::unique_ptr<std::thread> send_thread;
     std::chrono::milliseconds get_sender_time;
@@ -361,15 +370,18 @@ struct PublisherTest
 
   void send(size_t run_number)
   {
+    TLOG_DEBUG(7) << "Getting QuotaReached receiver and adding callback";
     auto start = std::chrono::steady_clock::now();
     auto quota_receiver = dunedaq::get_iom_receiver<QuotaReached>(config.get_publisher_quota_name());
     std::unordered_map<size_t, std::set<size_t>> completed_receiver_tracking;
     std::mutex tracking_mutex;
     auto quota_callback = [&](QuotaReached& msg) {
-      TLOG_DEBUG(5) << "Received QuotaReached message from app " << msg.sender_id << ", group " << msg.group_id
+      TLOG_DEBUG(4) << "Received QuotaReached message from app " << msg.sender_id << ", group " << msg.group_id
                     << ", conn " << msg.conn_id << " for run " << msg.run_number;
 
-      if (msg.run_number == run_number) {
+      if (msg.run_number == run_number && msg.sender_id == config.my_id) {
+        TLOG_DEBUG(4) << "Informing sender for " << config.get_connection_name(config.my_id, msg.group_id, msg.conn_id)
+                      << " that it can stop";
         std::lock_guard<std::mutex> lk(tracking_mutex);
         completed_receiver_tracking[msg.group_id].insert(msg.conn_id);
       }
@@ -377,6 +389,7 @@ struct PublisherTest
     quota_receiver->add_callback(quota_callback);
     auto after_control = std::chrono::steady_clock::now();
 
+    TLOG_DEBUG(7) << "Setting up PublisherInfo objects";
     for (size_t group = 0; group < config.num_groups; ++group) {
       for (size_t conn = 0; conn < config.num_connections_per_group; ++conn) {
         auto info = std::make_shared<PublisherInfo>(group, conn);
@@ -384,6 +397,8 @@ struct PublisherTest
       }
     }
     auto after_counters = std::chrono::steady_clock::now();
+
+    TLOG_DEBUG(7) << "Getting publisher objects for each connection";
     std::for_each(std::execution::par_unseq,
                   std::begin(publishers),
                   std::end(publishers),
@@ -398,21 +413,21 @@ struct PublisherTest
 
     auto after_senders = std::chrono::steady_clock::now();
 
+    TLOG_DEBUG(7) << "Starting publish threads";
     std::for_each(std::execution::par_unseq,
                   std::begin(publishers),
                   std::end(publishers),
                   [&](std::shared_ptr<PublisherInfo> info) {
                     info->send_thread.reset(new std::thread([=, &completed_receiver_tracking, &tracking_mutex]() {
                       bool complete_received = false;
-                      size_t seq = 0;
                       while (!complete_received) {
-                        TLOG_DEBUG(4) << "Sending message " << seq << " with size " << config.message_size_kb * 1024
+                        TLOG_DEBUG(4) << "Sending message " << info->messages_sent << " with size " << config.message_size_kb * 1024
                                       << " bytes to connection "
                                       << config.get_connection_name(config.my_id, info->group_id, info->conn_id);
 
-                        Data d(seq, config.my_id, info->group_id, info->conn_id, config.message_size_kb * 1024);
-                        info->sender->send(std::move(d), Sender::s_block);
-
+                        Data d(info->messages_sent, config.my_id, info->group_id, info->conn_id, config.message_size_kb * 1024);
+                        info->sender->try_send(std::move(d), std::chrono::milliseconds(config.send_interval_ms));
+                        ++info->messages_sent;
                         {
                           std::lock_guard<std::mutex> lk(tracking_mutex);
                           if (completed_receiver_tracking.count(info->group_id) &&
@@ -420,24 +435,31 @@ struct PublisherTest
                             complete_received = true;
                           }
                         }
+
+                        std::this_thread::sleep_for(std::chrono::milliseconds(config.send_interval_ms));
                       }
                     }));
                   });
     auto after_send_start = std::chrono::steady_clock::now();
 
+    TLOG_DEBUG(7) << "Joining send threads";
     for (auto& sender : publishers) {
       sender->send_thread->join();
       sender->send_thread.reset(nullptr);
     }
     auto after_join = std::chrono::steady_clock::now();
 
+    TLOG_DEBUG(7) << "Removing QuotaReached callback";
     quota_receiver->remove_callback();
     auto after_control_recvd = std::chrono::steady_clock::now();
 
+    TLOG_DEBUG(7) << "Calculating statistics";
     int min_get_time = std::numeric_limits<int>::max();
     int max_get_time = 0;
     int sum_get_time = 0;
     for (auto& info : publishers) {
+      TLOG_DEBUG(8) << "Published " << info->messages_sent << " messages to connection "
+                    << config.get_connection_name(config.my_id, info->group_id, info->conn_id);
       auto get_time = info->get_sender_time.count();
       if (get_time > max_get_time)
         max_get_time = get_time;
@@ -448,6 +470,7 @@ struct PublisherTest
     double average_get_time = static_cast<double>(sum_get_time) / static_cast<double>(publishers.size());
 
     std::string info_file_name = config.info_file_base + "_sender.csv";
+    TLOG_DEBUG(7) << "Writing to output CSV file " << info_file_name;
 
     struct stat buffer;
     bool file_exists = stat(info_file_name.c_str(), &buffer) == 0;
@@ -474,6 +497,8 @@ struct PublisherTest
        << min_get_time << "," << max_get_time << "," << average_get_time << std::endl;
 
     publishers.clear();
+
+    TLOG_DEBUG(7) << "send() done";
   }
 };
 
@@ -508,6 +533,7 @@ main(int argc, char* argv[])
     "publish_interval,i",
     po::value<int>(&config.publish_interval),
     "Interval, in ms, for ConfigClient to re-publish connection info")(
+    "send_interval,I", po::value<size_t>(&config.send_interval_ms), "Interval, in ms, for Publishers to send messages")(
     "output_file_base,o",
     po::value<std::string>(&config.info_file_base),
     "Base name for output info file (will have _sender.csv or _receiver.csv appended)")(
@@ -554,7 +580,7 @@ main(int argc, char* argv[])
 
   TLOG() << (is_subscriber ? "Subscriber " : "Publisher ") << config.my_id << ": "
          << "Configuring IOManager";
-  config.configure_iomanager(is_subscriber);
+  config.configure_iomanager(!is_subscriber);
 
   auto subscriber = std::make_unique<dunedaq::iomanager::SubscriberTest>(config);
   auto publisher = std::make_unique<dunedaq::iomanager::PublisherTest>(config);
